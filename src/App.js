fadc4e31523be15c4793535c1a3266144df9d710
Jerker Bäckström <jerker.backstrom@ims.fi>
2019-10-08 15:40:18 +0300

    Implement functionality to show games per team in table format

--- src/App.js
+++ src/App.js
@@ -1,9 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import TEAMS from './teams';
 
 function App() {
-  return (
-    <div>NHL Games</div>
-  );
+  const [games, setGames] = useState([]);
+  const [dates, setDates] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get('https://statsapi.web.nhl.com/api/v1/schedule?startDate=2019-10-08&endDate=2019-10-21');
+      const g = [];
+      const d = [];
+
+      for (let i = 0; i < TEAMS.length; i++) {
+        g[i] = [];
+        for (let j = 0; j < response.data.dates.length; j++) {
+          g[i][j] = '';
+        }
+      }
+
+      response.data.dates.forEach((date, dateIndex) => {
+        d.push(date.date);
+
+        date.games.forEach(game => {
+          const homeTeamIndex = TEAMS.findIndex(team => team.id === game.teams.home.team.id);
+          const awayTeamIndex = TEAMS.findIndex(team => team.id === game.teams.away.team.id);
+          g[homeTeamIndex][dateIndex] = { gamePk: game.gamePk, against: TEAMS[awayTeamIndex].shortName };
+          g[awayTeamIndex][dateIndex] = { gamePk: game.gamePk, against: TEAMS[homeTeamIndex].shortName };
+        });
+      });
+      setGames(g);
+      setDates(d);
+    })();
+  }, []);
+
+  const renderHeaderRow = () => {
+    return (
+      <tr>
+        <th>TEAM</th>
+        {dates.map(date => {
+          return <th key={date}>{date}</th>;
+        })}
+      </tr>
+    );
+  };
+
+  const renderGameRows = () => {
+    return TEAMS.map((team, index) => {
+      return (
+        <tr>
+          <td>{team.shortName}</td>
+          {games[index].map(game => (
+            <td key={game.gamePk}>{game.against}</td>
+          ))}
+        </tr>
+      );
+    });
+  };
+
+  if (games.length > 0) {
+    return (
+      <div>
+        <h1>NHL Games</h1>
+        <div>
+          <table>
+            <thead>{renderHeaderRow()}</thead>
+            <tbody>{renderGameRows()}</tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+
+  return <div>No games</div>;
 }
 
 export default App;
---
+++ src/teams.js
@@ -0,0 +1,59 @@
+const TEAMS = [
+  {
+    id: 24,
+    shortName: 'ANA',
+    name: 'Anaheim Ducks'
+  },
+  {
+    id: 53,
+    shortName: 'ARI',
+    name: 'Arizona Coyotes'
+  },
+  {
+    id: 6,
+    shortName: 'BOS',
+    name: 'Boston Bruins'
+  },
+  { id: 7, shortName: 'BUF', name: 'Buffalo Sabres' },
+  {
+    id: 20,
+    shortName: 'CGY',
+    name: 'Calgary Flames'
+  },
+  {
+    id: 12,
+    shortName: 'CAR',
+    name: 'Carolina Hurricanes'
+  },
+  { id: 16, shortName: 'CHI', name: 'Chicago Blackhawks' },
+  { id: 21, shortName: 'COL', name: 'Colorado Avalanche' },
+  { id: 29, shortName: 'CBJ', name: 'Columbus Blue Jackets' },
+  {
+    id: 25,
+    shortName: 'DAL',
+    name: 'Dallas Stars'
+  },
+  { id: 17, shortName: 'DET', name: 'Detroit Red Wings' },
+  { id: 22, shortName: 'EDM', name: 'Edmonton Oilers' },
+  { id: 13, shortName: 'FLA', name: 'Florida Panthers' },
+  { id: 26, shortName: 'LAK', name: 'Los Angeles Kings' },
+  { id: 30, shortName: 'MIN', name: 'Minnesota Wild' },
+  { id: 8, shortName: 'MTL', name: 'Montréal Canadiens' },
+  { id: 18, shortName: 'NSH', name: 'Nashville Predators' },
+  { id: 1, shortName: 'NJD', name: 'New Jersey Devils' },
+  { id: 2, shortName: 'NYI', name: 'New York Islanders' },
+  { id: 3, shortName: 'NYR', name: 'New York Rangers' },
+  { id: 9, shortName: 'OTT', name: 'Ottawa Senators' },
+  { id: 4, shortName: 'PHI', name: 'Philadelphia Flyers' },
+  { id: 5, shortName: 'PIT', name: 'Pittsburgh Penguins' },
+  { id: 28, shortName: 'SJS', name: 'San Jose Sharks' },
+  { id: 19, shortName: 'STL', name: 'St. Louis Blues' },
+  { id: 14, shortName: 'TBL', name: 'Tampa Bay Lightning' },
+  { id: 10, shortName: 'TOR', name: 'Toronto Maple Leafs' },
+  { id: 23, shortName: 'VAN', name: 'Vancouver Canucks' },
+  { id: 54, shortName: 'VGK', name: 'Vegas Golden Knights' },
+  { id: 15, shortName: 'WSH', name: 'Washington Capitals' },
+  { id: 52, shortName: 'WPG', name: 'Winnipeg Jets' }
+];
+
+export default TEAMS;
