c6ea48a997656117dcd5cc73bf877721419f6940
Jerker Bäckström <jerker.backstrom@ims.fi>
2019-10-11 15:26:28 +0300

    Add react daypicker to help set date range for the schedule

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import moment from 'moment';
+import { formatDate, parseDate } from 'react-day-picker/moment';
 import TEAMS from './teams';
 
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
+import './style.css';
+
 function App() {
+  const toEl = useRef(null);
+  const today = moment();
+  const weekFromNow = moment().add(7, 'days');
+  const [from, setFrom] = useState(today.format('YYYY-MM-DD'));
+  const [to, setTo] = useState(weekFromNow.format('YYYY-MM-DD'));
   const [games, setGames] = useState([]);
   const [dates, setDates] = useState([]);
 
   useEffect(() => {
     (async () => {
-      const response = await axios.get('https://statsapi.web.nhl.com/api/v1/schedule?startDate=2019-10-08&endDate=2019-10-27');
+      const response = await axios.get(`https://statsapi.web.nhl.com/api/v1/schedule?startDate=${from}&endDate=${to}`);
       const g = [];
       const d = [];
 
@@ -32,7 +43,7 @@ function App() {
       setGames(g);
       setDates(d);
     })();
-  }, []);
+  }, [from, to]);
 
   const renderHeaderRow = () => (
     <tr>
@@ -58,11 +69,61 @@ function App() {
       );
     });
 
+  const handleFromChange = day => {
+    const newFrom = moment(day).format('YYYY-MM-DD');
+    if (moment(newFrom).isBefore(to)) {
+      setFrom(newFrom);
+    }
+  };
+
+  const handleToChange = day => {
+    const newTo = moment(day).format('YYYY-MM-DD');
+    if (moment(newTo).isAfter(from)) {
+      setTo(newTo);
+    }
+  };
+
+  const toDate = moment(to).toDate();
+  const fromDate = moment(from).toDate();
+  const modifiers = { start: fromDate, end: toDate };
+
   if (games.length > 0) {
     return (
       <div>
-        <h1>NHL Games</h1>
+        <h1>NHL Schedule</h1>
         <div>
+          <div>
+            <DayPickerInput
+              value={fromDate}
+              format="LL"
+              formatDate={formatDate}
+              parseDate={parseDate}
+              onDayChange={handleFromChange}
+              dayPickerProps={{
+                selectedDays: [fromDate, { from: fromDate, to: toDate }],
+                disabledDays: { after: toDate },
+                toMonth: toDate,
+                modifiers,
+                onDayClick: () => toEl.current.getInput().focus()
+              }}
+            />
+            {' - '}
+            <DayPickerInput
+              ref={toEl}
+              value={toDate}
+              format="LL"
+              formatDate={formatDate}
+              parseDate={parseDate}
+              onDayChange={handleToChange}
+              dayPickerProps={{
+                selectedDays: [fromDate, { from: fromDate, to: toDate }],
+                disabledDays: { before: fromDate },
+                modifiers,
+                month: fromDate,
+                fromMonth: fromDate
+              }}
+            />
+          </div>
           <table>
             <thead>{renderHeaderRow()}</thead>
             <tbody>{renderGameRows()}</tbody>
